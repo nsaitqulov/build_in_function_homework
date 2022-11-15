d979c1d53258df447d309066a9a8d6fa776b44f0
Jalilov <javohirjalilov777@gmail.com>
2022-11-15 19:37:41 +0500

    done

---
+++ build_func01.py
@@ -0,0 +1,10 @@
+def main(n):
+    """
+    Given a argument called 'n' type of int , return its absolute value
+    Args:
+        n: int
+    Returns:
+        absolute value: int
+    """
+
+    return 
---
+++ build_func04.py
@@ -0,0 +1,9 @@
+def main(n):
+    """
+    Given a argument called 'n' type of int , calculate the value of expression and return result:
+    Args:
+        n: int
+    Returns:
+        result : float
+    """
+    return 
